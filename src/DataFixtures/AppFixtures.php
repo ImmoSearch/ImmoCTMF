653a8b84c270befd10b4fc11a3b2dc2a10a02119
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2023-11-11 21:19:34 +0100

    Lancement des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,26 +2,45 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Bien;
-use App\Repository\AnnonceRepository;
+use App\Entity\Annonce;
 use Doctrine\Persistence\ObjectManager;
-use App\Repository\UtilisateurRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $fichierBienCsv=fopen(__DIR__."/bien.csv","r");
-        while (!feof($fichierBienCsv)) {
-            $lesBiens[]=fgetcsv($fichierBienCsv);
-        }
-        fclose($fichierBienCsv);
 
-        foreach ($lesBiens as $value) {
-            $bien = new Bien();
-            $bien ->setId()
+        // Les images avec unsplash
+        function getRandomUnsplashImageUrl($category) {
+            $query = urlencode($category);
+            $unsplashUrl = "https://source.unsplash.com/random?{$query}";
+            return $unsplashUrl;
         }
+
+        // Les annonces factices
+        $fichierAnnonceCsv = fopen(__DIR__ . "/annonce.csv", "r");
+        while (!feof($fichierAnnonceCsv)) {
+            $lesAnnonces[] = fgetcsv($fichierAnnonceCsv);
+        }
+        fclose($fichierAnnonceCsv);
+
+        foreach ($lesAnnonces as $value) {
+            // Vérifiez si $value est un tableau
+            if (is_array($value) && count($value) >= 6) {
+                $annonce = new Annonce();
+                $annonce->setId(intval($value[0] ?? 0))
+                        ->setOrganisme($value[1] ?? '')
+                        ->setNom($value[2] ?? '')
+                        ->setVille($value[3] ?? '')
+                        ->setCp($value[5] ?? '')
+                        ->setDescription($value[4] ?? '')
+                        ->setImage(getRandomUnsplashImageUrl("logo"))
+                        ->setImage(getRandomUnsplashImageUrl("appartements"));
+                $manager->persist($annonce);
+            }
+        }
+
         $manager->flush();
     }
 }
