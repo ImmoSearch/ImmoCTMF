921e96a38f9c8164ff38970890872e47b2b6fa49
Fedi-EL-Manoubi <>
2023-11-20 11:07:09 +0100

    modif controller biens

--- src/Controller/ListeBiensController.php
+++ src/Controller/ListeBiensController.php
@@ -1,9 +1,11 @@
 <?php
 
+// src/Controller/ListeBiensController.php
+
 namespace App\Controller;
 
-use App\Entity\ListeBiens;
-use App\Repository\ListeBiensRepository; 
+use App\Entity\biens;
+use App\Repository\BiensRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class ListeBiensController extends AbstractController
 {
     /**
-     * @Route("/Biens", name="biens", methods={"GET"})
+     * @Route("/biens", name="biens", methods={"GET"})
      */
-    public function listeBiens(listeBiensRepository $repo): Response
+    public function listeBiens(BiensRepository $repo)
     {
-        $biens = $repo->findAll(); 
+        $biens = $repo->findAll();
         return $this->render('liste_biens/listeBiens.html.twig', [
-            'lesBiens' => $biens
+            'LesBiens' => $biens
         ]);
     }
 }
+
+
