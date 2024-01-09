291b3169b1d4c54e151dc11fffa0b1fa1aad4185
Fédi <123569039+Fedi78@users.noreply.github.com>
2024-01-10 00:51:03 +0100

    video  30min pb mailhog

--- migrations/Version20240109223916.php
+++ migrations/Version20240109223916.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231128100200 extends AbstractMigration
+final class Version20240109223916 extends AbstractMigration
 {
     public function getDescription(): string
     {
